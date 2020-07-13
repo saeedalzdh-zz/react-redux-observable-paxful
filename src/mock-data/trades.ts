2c8868ab60efa431679a99fa365652f97f944b3c
Saeed Alizadeh <saeed.alizadeh@pipedrive.com>
2020-07-13 09:12:46 +0300

    added mock data

---
+++ src/mock-data/trades.ts
@@ -0,0 +1,219 @@
+export default [
+	{
+		id: 1,
+		trader: {
+			id: 1,
+			name: 'Chanaar',
+			reputation: {
+				positive: 37,
+				negetive: 1
+			},
+			avatar: '/images/avatar.jpeg'
+		},
+		title: 'Amazon Gift Card',
+		type: 'buying',
+		status: 'paid',
+		unread: true,
+		amount: 77,
+		currency: 'usd',
+		hash: '45ajs34',
+		createdAt: '2020-07-11T17:55:25+00:00',
+		updatedAt: '2020-07-11T17:55:25+00:00',
+		messages: [
+			{
+				id: 1,
+				content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis voluptatibus exercitationem dolores culpa ad, nobis voluptate dolore incidunt et blanditiis necessitatibus in assumenda aperiam corporis dicta unde, cupiditate ipsa quod?',
+				createdAt: '2020-07-11T17:55:25+00:00',
+				owner: 'buyer'
+			},
+			{
+				id: 2,
+				content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis voluptatibus exercitationem dolores culpa ad, nobis voluptate dolore incidunt et blanditiis necessitatibus in assumenda aperiam corporis dicta unde, cupiditate ipsa quod?',
+				createdAt: '2020-07-11T17:55:25+00:00',
+				owner: 'buyer'
+			},
+			{
+				id: 3,
+				content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis voluptatibus exercitationem dolores culpa ad, nobis voluptate dolore incidunt et blanditiis necessitatibus in assumenda aperiam corporis dicta unde, cupiditate ipsa quod?',
+				createdAt: '2020-07-11T17:55:25+00:00',
+				owner: 'seller'
+			},
+		]
+	},
+	{
+		id: 2,
+		trader: {
+			id: 1,
+			name: 'Chanaar',
+			reputation: {
+				positive: 37,
+				negetive: 1
+			},
+			avatar: '/images/avatar.jpeg'
+		},
+		title: 'Paypal Payment',
+		type: 'buying',
+		status: 'not paid',
+		unread: true,
+		amount: 22,
+		currency: 'usd',
+		hash: 'po3421ff',
+		createdAt: '2020-07-11T17:55:25+00:00',
+		updatedAt: '2020-07-11T17:55:25+00:00',
+		messages: [
+			{
+				id: 1,
+				content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis voluptatibus exercitationem dolores culpa ad, nobis voluptate dolore incidunt et blanditiis necessitatibus in assumenda aperiam corporis dicta unde, cupiditate ipsa quod?',
+				createdAt: '2020-07-11T17:55:25+00:00',
+				owner: 'buyer'
+			},
+			{
+				id: 2,
+				content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis voluptatibus exercitationem dolores culpa ad, nobis voluptate dolore incidunt et blanditiis necessitatibus in assumenda aperiam corporis dicta unde, cupiditate ipsa quod?',
+				createdAt: '2020-07-11T17:55:25+00:00',
+				owner: 'seller'
+			},
+			{
+				id: 3,
+				content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis voluptatibus exercitationem dolores culpa ad, nobis voluptate dolore incidunt et blanditiis necessitatibus in assumenda aperiam corporis dicta unde, cupiditate ipsa quod?',
+				createdAt: '2020-07-11T17:55:25+00:00',
+				owner: 'buyer'
+			},
+		]
+	},
+	{
+		id: 3,
+		trader: {
+			id: 1,
+			name: 'Chanaar',
+			reputation: {
+				positive: 37,
+				negetive: 1
+			},
+			avatar: '/images/avatar.jpeg'
+		},
+		title: 'Amazon Gift Card',
+		type: 'buying',
+		status: 'paid',
+		unread: true,
+		amount: 25,
+		currency: 'usd',
+		hash: 'fh3943l',
+		createdAt: '2020-07-11T17:55:25+00:00',
+		updatedAt: '2020-07-11T17:55:25+00:00',
+		messages: [
+			{
+				id: 1,
+				content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis voluptatibus exercitationem dolores culpa ad, nobis voluptate dolore incidunt et blanditiis necessitatibus in assumenda aperiam corporis dicta unde, cupiditate ipsa quod?',
+				createdAt: '2020-07-11T17:55:25+00:00',
+				owner: 'seller'
+			},
+			{
+				id: 2,
+				content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis voluptatibus exercitationem dolores culpa ad, nobis voluptate dolore incidunt et blanditiis necessitatibus in assumenda aperiam corporis dicta unde, cupiditate ipsa quod?',
+				createdAt: '2020-07-11T17:55:25+00:00',
+				owner: 'seller'
+			},
+			{
+				id: 3,
+				content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis voluptatibus exercitationem dolores culpa ad, nobis voluptate dolore incidunt et blanditiis necessitatibus in assumenda aperiam corporis dicta unde, cupiditate ipsa quod?',
+				createdAt: '2020-07-11T17:55:25+00:00',
+				owner: 'buyer'
+			},
+		]
+	},
+	{
+		id: 4,
+		trader: {
+			id: 1,
+			name: 'Chanaar',
+			reputation: {
+				positive: 37,
+				negetive: 1
+			},
+			avatar: '/images/avatar.jpeg'
+		},
+		title: 'Paypal Payment',
+		type: 'buying',
+		status: 'not paid',
+		unread: false,
+		amount: 46,
+		currency: 'usd',
+		hash: '83udhj3',
+		createdAt: '2020-07-11T17:55:25+00:00',
+		updatedAt: '2020-07-11T17:55:25+00:00',
+		messages: [
+			{
+				id: 1,
+				content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis voluptatibus exercitationem dolores culpa ad, nobis voluptate dolore incidunt et blanditiis necessitatibus in assumenda aperiam corporis dicta unde, cupiditate ipsa quod?',
+				createdAt: '2020-07-11T17:55:25+00:00',
+				owner: 'buyer'
+			},
+			{
+				id: 2,
+				content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis voluptatibus exercitationem dolores culpa ad, nobis voluptate dolore incidunt et blanditiis necessitatibus in assumenda aperiam corporis dicta unde, cupiditate ipsa quod?',
+				createdAt: '2020-07-11T17:55:25+00:00',
+				owner: 'buyer'
+			},
+			{
+				id: 3,
+				content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis voluptatibus exercitationem dolores culpa ad, nobis voluptate dolore incidunt et blanditiis necessitatibus in assumenda aperiam corporis dicta unde, cupiditate ipsa quod?',
+				createdAt: '2020-07-11T17:55:25+00:00',
+				owner: 'seller'
+			},
+			{
+				id: 4,
+				content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis voluptatibus exercitationem dolores culpa ad, nobis voluptate dolore incidunt et blanditiis necessitatibus in assumenda aperiam corporis dicta unde, cupiditate ipsa quod?',
+				createdAt: '2020-07-11T17:55:25+00:00',
+				owner: 'seller'
+			},
+		]
+	},
+	{
+		id: 5,
+		trader: {
+			id: 1,
+			name: 'Chanaar',
+			reputation: {
+				positive: 37,
+				negetive: 1
+			},
+			avatar: '/images/avatar.jpeg'
+		},
+		title: 'iTunes Gift Card',
+		type: 'buying',
+		status: 'not paid',
+		unread: false,
+		amount: 33,
+		currency: 'usd',
+		hash: '45kjkwe',
+		createdAt: '2020-07-11T17:55:25+00:00',
+		updatedAt: '2020-07-11T17:55:25+00:00',
+		messages: [
+			{
+				id: 1,
+				content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis voluptatibus exercitationem dolores culpa ad, nobis voluptate dolore incidunt et blanditiis necessitatibus in assumenda aperiam corporis dicta unde, cupiditate ipsa quod?',
+				createdAt: '2020-07-11T17:55:25+00:00',
+				owner: 'buyer'
+			},
+			{
+				id: 2,
+				content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis voluptatibus exercitationem dolores culpa ad, nobis voluptate dolore incidunt et blanditiis necessitatibus in assumenda aperiam corporis dicta unde, cupiditate ipsa quod?',
+				createdAt: '2020-07-11T17:55:25+00:00',
+				owner: 'buyer'
+			},
+			{
+				id: 3,
+				content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis voluptatibus exercitationem dolores culpa ad, nobis voluptate dolore incidunt et blanditiis necessitatibus in assumenda aperiam corporis dicta unde, cupiditate ipsa quod?',
+				createdAt: '2020-07-11T17:55:25+00:00',
+				owner: 'seller'
+			},
+			{
+				id: 4,
+				content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis voluptatibus exercitationem dolores culpa ad, nobis voluptate dolore incidunt et blanditiis necessitatibus in assumenda aperiam corporis dicta unde, cupiditate ipsa quod?',
+				createdAt: '2020-07-11T17:55:25+00:00',
+				owner: 'seller'
+			},
+		]
+	},
+];
